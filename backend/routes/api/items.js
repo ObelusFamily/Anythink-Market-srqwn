afaffdaa56f41d06883a33e0dc66b15b3cf26f45
ozzzozo <{ID}+{username}@users.noreply.github.com>
2022-08-03 21:38:13 +0300

    Missing Image Bug Fix

--- backend/routes/api/items.js
+++ backend/routes/api/items.js
@@ -148,6 +148,10 @@ router.post("/", auth.required, function(req, res, next) {
 
       item.seller = user;
 
+      if(item.image === "") {
+        item.image = "http://localhost:3001/placeholder.png";
+      }
+
       return item.save().then(function() {
         sendEvent('item_created', { item: req.body.item })
         return res.json({ item: item.toJSONFor(user) });
